6bdfa9a7403c5e4f5a3abfab378fa463145e1785
jun <xodlf100@gmail.com>
2025-11-12 23:37:14 +0900

    feat add global-layout

---
+++ onebite-next/section02/src/components/global-layout.tsx
@@ -0,0 +1,17 @@
+import {ReactNode} from "react";
+import Link from "next/link";
+import style from "./global-layout.module.css";
+
+export default function GlobalLayout({ children }: {
+  children: ReactNode;
+}) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={'/'}>✅ ONEBITE BOOKS</Link>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @yejun95</footer>
+    </div>
+  )
+}
