77e7ea7a29c0717de8f694cc3784f073ec2b6ec0
jun <xodlf100@gmail.com>
2025-11-13 00:07:19 +0900

    feat add searchbar layout

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,5 +1,11 @@
 // CSS Module
 import style from "./index.module.css";
+import SearchableLayout from "@/components/searchable-layout";
+import {ReactNode} from "react";
 export default function Home() {
   return <h1 className={style.h1}>인덱스</h1>;
+}
+
+Home.getLayout = (page:ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import {ReactNode} from "react";
+import SearchableLayout from "@/components/searchable-layout";
 
 export default function Page() {
   const router = useRouter();
@@ -6,4 +8,8 @@ export default function Page() {
   const { q } = router.query;
 
   return <h1>Search {q}</h1>
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
 }
