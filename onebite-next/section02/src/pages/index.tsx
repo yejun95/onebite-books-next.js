495e005370600fe4340147c21010ea8668052579
jun <xodlf100@gmail.com>
2025-11-18 00:04:57 +0900

    feat CSR => SSR 전환

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -2,39 +2,40 @@
 import SearchableLayout from "@/components/searchable-layout";
 import {ReactNode} from "react";
 import style from "./index.module.css"
-import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
 import {InferGetServerSidePropsType} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
 // 약속된 이름의 함수: SSR
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+export const getServerSideProps = async() => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ])
 
-  const data = "hello";
-
-  // 무조건 props 프로퍼티 객체로 반환해야함
   return {
     props: {
-      data
+      allBooks,
+      recoBooks
     }
   }
 };
 
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
-  console.log(data)
 
   return (
       <div className={style.container}>
         <section>
           <h3>지금 추천하는 도서</h3>
-          {books.map((book) => (
+          {recoBooks.map((book) => (
             <BookItem key={book.id} {...book}/>
           ))}
         </section>
         <section>
           <h3>등록된 모든 도서</h3>
-          {books.map((book) => (
+          {allBooks.map((book) => (
             <BookItem key={book.id} {...book}/>
           ))}
         </section>
