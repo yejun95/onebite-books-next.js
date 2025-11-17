b009267010d5b0f8aad7bd2628053400377be756
jun <xodlf100@gmail.com>
2025-11-17 23:26:13 +0900

    feat getSserverSideProps

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -4,8 +4,26 @@ import {ReactNode} from "react";
 import style from "./index.module.css"
 import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
+import {InferGetServerSidePropsType} from "next";
 
-export default function Home() {
+// 약속된 이름의 함수: SSR
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+
+  const data = "hello";
+
+  // 무조건 props 프로퍼티 객체로 반환해야함
+  return {
+    props: {
+      data
+    }
+  }
+};
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  console.log(data)
+
   return (
       <div className={style.container}>
         <section>
