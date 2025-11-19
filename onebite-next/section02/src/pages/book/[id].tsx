b5228ac4852f669e820f8521015aac0f09c8184b
jun <xodlf100@gmail.com>
2025-11-20 00:23:46 +0900

    feat SSG - 동적 경로 맵핑 방법 * pathVariable

--- onebite-next/section02/src/pages/book/[id].tsx
+++ onebite-next/section02/src/pages/book/[id].tsx
@@ -1,8 +1,35 @@
 import style from "./[id].module.css"
-import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
+import {GetStaticPropsContext, InferGetStaticPropsType} from "next";
 import fetchOneBook from "@/lib/fetch-one-books";
 
-export const getServerSideProps = async(context: GetServerSidePropsContext) => {
+// SSR
+// export const getServerSideProps = async(context: GetServerSidePropsContext) => {
+//
+//   const id = context.params!.id;
+//   const book = await fetchOneBook(Number(id));
+//
+//   return {
+//     props: {
+//       book
+//     }
+//   }
+// }
+
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      // pathVariable은 반드시 문자열로 지정
+      { params: { id: "1"} },
+      { params: { id: "2"} },
+      { params: { id: "3"} }
+    ],
+    // 일종의 대비책
+    fallback: false
+  }
+}
+
+// SSG
+export const getStaticProps = async(context: GetStaticPropsContext) => {
 
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
@@ -14,7 +41,7 @@ export const getServerSideProps = async(context: GetServerSidePropsContext) => {
   }
 }
 
-export default function Page({book}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page({book}: InferGetStaticPropsType<typeof getStaticProps>) {
 
   if (!book) return "문제가 발생했습니다. 다시 시도하세요."
 
