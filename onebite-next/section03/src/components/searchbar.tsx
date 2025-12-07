0fd00a92c7961ad86aa26af4b154ebc728d12981
jun <xodlf100@gmail.com>
2025-12-07 22:10:08 +0900

    ëapp router버전 ui 적용

--- onebite-next/section03/src/components/searchbar.tsx
+++ onebite-next/section03/src/components/searchbar.tsx
@@ -1,24 +1,43 @@
 "use client";
 
-import {useState} from "react";
-import {useRouter} from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
 
-export default function SearchBar() {
+export default function Searchbar() {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [search, setSearch] = useState("");
 
-  const onChangeSearch = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value)
-  }
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   const onSubmit = () => {
-    router.push(`/search?q=${search}`)
-  }
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
 
   return (
-    <div>
-      <input value={search} onChange={onChangeSearch}/>
+    <div className={style.container}>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
       <button onClick={onSubmit}>검색</button>
     </div>
-  )
+  );
 }
