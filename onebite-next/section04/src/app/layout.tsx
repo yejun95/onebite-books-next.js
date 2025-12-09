dda5a3bf3e8b4a5d601ad6d91c703e77a10caddd
jun <xodlf100@gmail.com>
2025-12-10 00:20:12 +0900

    Request Memoization test

--- onebite-next/section04/src/app/layout.tsx
+++ onebite-next/section04/src/app/layout.tsx
@@ -1,6 +1,25 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import {BookData} from "@/types";
+
+async function Footer () {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!response.ok) {
+    return <footer>제작 @yejun</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @yejun</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  )
+
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +34,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
