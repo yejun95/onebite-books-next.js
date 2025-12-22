8d5c570d2a85ef9dd1bed28e4a2871ab0db9923c
jun <xodlf100@gmail.com>
2025-12-22 22:55:00 +0900

    feat ㄱã리뷰 작성 후 리프레ㅣ시 - revalidatePath

--- onebite-next/section05_serverAction/src/actions/create-review.action.ts
+++ onebite-next/section05_serverAction/src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server"
 
+import {revalidatePath} from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -19,6 +21,10 @@ export async function createReviewAction(formData: FormData) {
       })
     })
     console.log(response.status);
+
+    // 서버측에게 페이지 재생성 요청하여 관련 컴포넌트가 재렌더링됨
+    // 서버 컴포넌트에서만 실행가능
+    revalidatePath(`/book/${bookId}`)
   } catch(err) {
     console.error(err);
     return;
