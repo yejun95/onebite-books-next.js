c919d2fe7189a641e000a8d6059e768ff900b2f1
jun <xodlf100@gmail.com>
2025-12-22 23:05:58 +0900

    feat revlidatePath 옵션, revalidateTag

--- onebite-next/section05_serverAction/src/actions/create-review.action.ts
+++ onebite-next/section05_serverAction/src/actions/create-review.action.ts
@@ -24,7 +24,21 @@ export async function createReviewAction(formData: FormData) {
 
     // 서버측에게 페이지 재생성 요청하여 관련 컴포넌트가 재렌더링됨
     // 서버 컴포넌트에서만 실행가능
-    revalidatePath(`/book/${bookId}`)
+
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath(`/book/[id]`, "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+
+    // 4. 모든 데이터 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증 -> 데이터 fetch 시 캐시 인수로 적용했었던 부분에 { next: { tags : [이부분에 입력] } } 적용
+    // revalidateTag(이부분에 입력);
   } catch(err) {
     console.error(err);
     return;
