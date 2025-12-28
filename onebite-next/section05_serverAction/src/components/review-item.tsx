e9526cd91924428b38e5c63538d6c3d98feea8a5
jun <xodlf100@gmail.com>
2025-12-28 22:03:29 +0900

    feat useServerAction을 이용한 리뷰ã 삭제 기능 구현

--- onebite-next/section05_serverAction/src/components/review-item.tsx
+++ onebite-next/section05_serverAction/src/components/review-item.tsx
@@ -1,5 +1,6 @@
 import style from "./review-item.module.css"
 import {ReviewData} from "@/types";
+import ReviewItemDeleteButton from "@/components/review-item-delete-button";
 
 export default function ReviewItem({
   id,
@@ -14,7 +15,9 @@ export default function ReviewItem({
       <div className={style.content}>{content}</div>
       <div className={style.bottom_container}>
         <div className={style.date}>{new Date(createdAt).toLocaleString()}</div>
-        <div className={style.delete_btn}>삭제하기</div>
+        <div className={style.delete_btn}>
+          <ReviewItemDeleteButton reviewId={id} bookId={bookId}/>
+        </div>
       </div>
     </div>
   )
